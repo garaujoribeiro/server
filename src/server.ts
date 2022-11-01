e3e223aab604cdcf3e92e1ddd9d95e53e73d12f6
Gabriel Ribeiro <gabriel.dev@h4money.com.br>
2022-11-01 17:35:30 -0300

    criando db

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,36 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
 
 const bootstrap = async () => {
   const fastify = Fastify({
     logger: true,
   })
 
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   // http://localhost:3333/
 
-  fastify.get(('/pools/count'), () => {
-    return { count: 0 }
+  fastify.get(('/pools/count'), async () => {
+    const count = await prisma.pool.count({
+      where: {
+        code: {
+          startsWith: 'T'
+        }
+      }
+    })
+    return { count }
   })
 
 
 
-  await fastify.listen({ port: 3333 })
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
