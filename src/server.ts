98c287e0c1dda9730e98d945591dc56a7912347f
Gabriel Ribeiro <gabriel.dev@h4money.com.br>
2022-11-01 19:05:47 -0300

    rotas iniciais

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
-
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 const prisma = new PrismaClient({
   log: ['query'],
 })
@@ -18,14 +19,37 @@ const bootstrap = async () => {
   // http://localhost:3333/
 
   fastify.get(('/pools/count'), async () => {
-    const count = await prisma.pool.count({
-      where: {
-        code: {
-          startsWith: 'T'
-        }
+    const count = await prisma.pool.count()
+    return { count }
+  })
+
+  fastify.get(('/users/count'), async () => {
+    const count = await prisma.user.count()
+    return { count }
+  })
+
+  fastify.get(('/guesses/count'), async () => {
+    const count = await prisma.guess.count()
+    return { count }
+  })
+
+  fastify.post(('/pools'), async (req, res) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+    const { title } = createPoolBody.parse(req.body)
+
+    const generate = new ShortUniqueId({ length: 6 })
+
+    const code = generate()
+
+    await prisma.pool.create({
+      data: {
+        title, code
       }
     })
-    return { count }
+
+    return res.status(201).send({ code })
   })
 
 
